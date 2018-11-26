2a253133010e2f5c304b7846f816b4b8d20389f9
Sandeep Singh <sandeepsingh@ucreate.com>
2018-11-26 12:28:02 +0530

    aa

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 3000, function(){
+   console.log("Hello world");
+ });
