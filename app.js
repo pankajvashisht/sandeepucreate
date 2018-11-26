705dbee18184df93fb355bdf3c2161cc6e314af6
Sandeep Singh <sandeepsingh@ucreate.com>
2018-11-26 12:51:20 +0530

    as

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(process.env.PORT || 3000, function(){
-   console.log("Hello world");
- });
