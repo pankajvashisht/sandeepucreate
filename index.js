fe971cb52a7a800d3830611adbcaaeadc44ca63b
Sandeep Singh <sandeepsingh@ucreate.com>
2019-01-10 19:06:06 +0530

    Implemented rollbar

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.load();
 const express = require('express')
+const Rollbar = require('rollbar')
+
+var rollbar = new Rollbar(process.env.ROLLBAR_ACCESS_TOKEN);
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/error', (req, res) =>{
+    try {
+        throw new Error('something bad happened')
+    } catch (error) {
+        rollbar.error(error)
+    }
+    res.send('Error posted to rollbar')
+ })
+
+app.use(rollbar.errorHandler());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
